cb7c3cc0dee0e84ba0e29fd76248beacf9b99f5b
Toni D'Angelo <tonydangelo123@gmail.com>
2019-01-23 19:34:46 +0100

    scaffold blog component

--- src/Blog.js
+++ src/Blog.js
@@ -13,11 +13,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Hidden from '@material-ui/core/Hidden';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-import Markdown from './Markdown';
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
+import CardGrid from './CardGrid';
 
 const styles = theme => ({
   layout: {
@@ -79,18 +75,6 @@ const styles = theme => ({
   },
 });
 
-const sections = [
-  'Technology',
-  'Design',
-  'Culture',
-  'Business',
-  'Politics',
-  'Opinion',
-  'Science',
-  'Health',
-  'Style',
-  'Travel',
-];
 
 const featuredPosts = [
   {
@@ -107,28 +91,16 @@ const featuredPosts = [
   },
 ];
 
-const posts = [post1, post2, post3];
-
-const archives = [
-  'March 2020',
-  'February 2020',
-  'January 2020',
-  'December 2019',
-  'November 2019',
-  'October 2019',
-  'September 2019',
-  'August 2019',
-  'July 2019',
-  'June 2019',
-  'May 2019',
-  'April 2019',
-];
-
-const social = ['GitHub', 'Twitter', 'Facebook'];
-
 function Blog(props) {
   const { classes } = props;
-
+  const name = 'daton'
+  const title = 'FullStack Web Developer'
+  const description = 'I build Web and Mobile applications, using the latest technologies, staying update with the coolest features and frameworks of Javascript'
+  const about = {
+    name: 'Tony D\'Angelo',
+    title: 'Web Developer',
+    description: 'I build Web and Mobile applications, using the latest technologies, staying update with the coolest features and frameworks of Javascript'
+  }
   return (
     <React.Fragment>
       <CssBaseline />
@@ -143,7 +115,7 @@ function Blog(props) {
             noWrap
             className={classes.toolbarTitle}
           >
-            Blog
+            {name}
           </Typography>
           <IconButton>
             <SearchIcon />
@@ -152,12 +124,8 @@ function Blog(props) {
             Sign up
           </Button>
         </Toolbar>
-        <Toolbar variant="dense" className={classes.toolbarSecondary}>
-          {sections.map(section => (
-            <Typography color="inherit" noWrap key={section}>
-              {section}
-            </Typography>
-          ))}
+        <Toolbar className={classes.toolbarMain}>
+          
         </Toolbar>
         <main>
           {/* Main featured post */}
@@ -166,32 +134,30 @@ function Blog(props) {
               <Grid item md={6}>
                 <div className={classes.mainFeaturedPostContent}>
                   <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                    Title of a longer featured blog post
+                    {title}
                   </Typography>
                   <Typography variant="h5" color="inherit" paragraph>
-                    Multiple lines of text that form the lede, informing new readers quickly and
-                    efficiently about what&apos;s most interesting in this post&apos;s contents…
+                    {description}
                   </Typography>
                 </div>
               </Grid>
             </Grid>
           </Paper>
           {/* End main featured post */}
-          {/* Sub featured posts */}
+          {/* About me */}
           <Grid container spacing={40} className={classes.cardGrid}>
-            {featuredPosts.map(post => (
-              <Grid item key={post.title} xs={12} md={6}>
+              <Grid item xs={12} md={12}>
                 <Card className={classes.card}>
                   <div className={classes.cardDetails}>
                     <CardContent>
                       <Typography component="h2" variant="h5">
-                        {post.title}
+                        {about.name}
                       </Typography>
                       <Typography variant="subtitle1" color="textSecondary">
-                        {post.date}
+                        {about.title}
                       </Typography>
                       <Typography variant="subtitle1" paragraph>
-                        {post.description}
+                        {about.description}
                       </Typography>
                       <Typography variant="subtitle1" color="primary">
                         Continue reading...
@@ -207,49 +173,9 @@ function Blog(props) {
                   </Hidden>
                 </Card>
               </Grid>
-            ))}
           </Grid>
-          {/* End sub featured posts */}
-          <Grid container spacing={40} className={classes.mainGrid}>
-            {/* Main content */}
-            <Grid item xs={12} md={8}>
-              <Typography variant="h6" gutterBottom>
-                From the Firehose
-              </Typography>
-              <Divider />
-              {posts.map(post => (
-                <Markdown className={classes.markdown} key={post.substring(0, 40)}>
-                  {post}
-                </Markdown>
-              ))}
-            </Grid>
-            {/* End main content */}
-            {/* Sidebar */}
-            <Grid item xs={12} md={4}>
-              <Paper elevation={0} className={classes.sidebarAboutBox}>
-                <Typography variant="h6" gutterBottom>
-                  About
-                </Typography>
-                <Typography>
-                  Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit
-                  amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
-                </Typography>
-              </Paper>
-              <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
-                Archives
-              </Typography>
-              {archives.map(archive => (
-                <Typography key={archive}>{archive}</Typography>
-              ))}
-              <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
-                Social
-              </Typography>
-              {social.map(network => (
-                <Typography key={network}>{network}</Typography>
-              ))}
-            </Grid>
-            {/* End sidebar */}
-          </Grid>
+          {/* About me end */}
+          <CardGrid />
         </main>
       </div>
       {/* Footer */}
