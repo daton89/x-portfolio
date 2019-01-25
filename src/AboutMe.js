9fc45251e76e0a6c86e57c4ba1dff4227a880ee4
Toni D'Angelo <tonydangelo123@gmail.com>
2019-01-25 20:09:26 +0100

    use data in about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,21 +7,26 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Hidden from "@material-ui/core/Hidden";
 import { withStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
 
 const styles = theme => ({
   card: {
     display: "flex"
   },
   cardDetails: {
-    flex: 1
+    flex: 1,
+    padding: '15px 30px'
+  },
+  divider: {
+    margin: '10px 0'
   },
   cardMedia: {
-    width: 160
+    width: 260
   }
 });
 
 const AboutMe = props => {
-  const { classes, name, title, description } = props;
+  const { classes, name, description, image } = props;
   return (
     <Grid container spacing={40} className={classes.cardGrid}>
       <Grid item xs={12} md={12}>
@@ -31,9 +36,7 @@ const AboutMe = props => {
               <Typography component="h2" variant="h5">
                 {name}
               </Typography>
-              <Typography variant="subtitle1" color="textSecondary">
-                {title}
-              </Typography>
+              <Divider className={classes.divider} />
               <Typography variant="subtitle1" paragraph>
                 {description}
               </Typography>
@@ -42,8 +45,8 @@ const AboutMe = props => {
           <Hidden xsDown>
             <CardMedia
               className={classes.cardMedia}
-              image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-              title="Image title"
+              image={image} // eslint-disable-line max-len
+              title="profile image"
             />
           </Hidden>
         </Card>
@@ -56,7 +59,8 @@ AboutMe.propTypes = {
   classes: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string,
 };
 
 export default withStyles(styles)(AboutMe);
