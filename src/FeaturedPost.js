25edbeffbfc5381649990457ac30de905555d4bf
Toni D'Angelo <tonydangelo123@gmail.com>
2019-01-25 16:29:47 +0100

    refactor featuredPost in a component

---
+++ src/FeaturedPost.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  mainFeaturedPost: {
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 4
+  },
+  mainFeaturedPostContent: {
+    padding: `${theme.spacing.unit * 6}px`,
+    [theme.breakpoints.up("md")]: {
+      paddingRight: 0
+    }
+  }
+});
+
+const FeaturedPost = props => {
+  const { classes, title, description } = props;
+  return (
+    <Paper className={classes.mainFeaturedPost}>
+      <Grid container>
+        <Grid item md={6}>
+          <div className={classes.mainFeaturedPostContent}>
+            <Typography
+              component="h1"
+              variant="h3"
+              color="inherit"
+              gutterBottom
+            >
+              {title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {description}
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+FeaturedPost.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+export default withStyles(styles)(FeaturedPost);
