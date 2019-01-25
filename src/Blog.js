c665a1288a1d89e83969ac3cb6288d3c79771f2a
Toni D'Angelo <tonydangelo123@gmail.com>
2019-01-25 11:07:52 +0100

    add socialToolbar skills and verticalTimeline to blog page

--- src/Blog.js
+++ src/Blog.js
@@ -1,106 +1,110 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
-import Button from '@material-ui/core/Button';
-import CardGrid from './CardGrid';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Hidden from "@material-ui/core/Hidden";
+import Button from "@material-ui/core/Button";
+import CardGrid from "./CardGrid";
+import SocialToolbar from "./SocialToolbar";
+import Skills from "./Skills";
+import VerticalTimeline from "./VerticalTimeline";
 
 const styles = theme => ({
   layout: {
-    width: 'auto',
+    width: "auto",
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
       width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
   },
   toolbarMain: {
-    borderBottom: `1px solid ${theme.palette.grey[300]}`,
+    borderBottom: `1px solid ${theme.palette.grey[300]}`
   },
   toolbarTitle: {
-    flex: 1,
+    flex: 1
   },
   toolbarSecondary: {
-    justifyContent: 'space-between',
+    justifyContent: "space-between"
   },
   mainFeaturedPost: {
     backgroundColor: theme.palette.grey[800],
     color: theme.palette.common.white,
-    marginBottom: theme.spacing.unit * 4,
+    marginBottom: theme.spacing.unit * 4
   },
   mainFeaturedPostContent: {
     padding: `${theme.spacing.unit * 6}px`,
-    [theme.breakpoints.up('md')]: {
-      paddingRight: 0,
-    },
+    [theme.breakpoints.up("md")]: {
+      paddingRight: 0
+    }
   },
   mainGrid: {
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 3
   },
   card: {
-    display: 'flex',
+    display: "flex"
   },
   cardDetails: {
-    flex: 1,
+    flex: 1
   },
   cardMedia: {
-    width: 160,
+    width: 160
   },
   markdown: {
-    padding: `${theme.spacing.unit * 3}px 0`,
+    padding: `${theme.spacing.unit * 3}px 0`
   },
   sidebarAboutBox: {
     padding: theme.spacing.unit * 2,
-    backgroundColor: theme.palette.grey[200],
+    backgroundColor: theme.palette.grey[200]
   },
   sidebarSection: {
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 3
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
     marginTop: theme.spacing.unit * 8,
-    padding: `${theme.spacing.unit * 6}px 0`,
-  },
+    padding: `${theme.spacing.unit * 6}px 0`
+  }
 });
-
 
 const featuredPosts = [
   {
-    title: 'Featured post',
-    date: 'Nov 12',
+    title: "Featured post",
+    date: "Nov 12",
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      "This is a wider card with supporting text below as a natural lead-in to additional content."
   },
   {
-    title: 'Post title',
-    date: 'Nov 11',
+    title: "Post title",
+    date: "Nov 11",
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-  },
+      "This is a wider card with supporting text below as a natural lead-in to additional content."
+  }
 ];
 
 function Blog(props) {
   const { classes } = props;
-  const name = 'daton'
-  const title = 'FullStack Web Developer'
-  const description = 'I build Web and Mobile applications, using the latest technologies, staying update with the coolest features and frameworks of Javascript'
+  const name = "daton";
+  const title = "FullStack Web Developer";
+  const description =
+    "I build Web and Mobile applications, using the latest technologies, staying update with the coolest features and frameworks of Javascript";
   const about = {
-    name: 'Tony D\'Angelo',
-    title: 'Web Developer',
-    description: 'I build Web and Mobile applications, using the latest technologies, staying update with the coolest features and frameworks of Javascript'
-  }
+    name: "Tony D'Angelo",
+    title: "Web Developer",
+    description:
+      "I build Web and Mobile applications, using the latest technologies, staying update with the coolest features and frameworks of Javascript"
+  };
   return (
     <React.Fragment>
       <CssBaseline />
@@ -124,16 +128,18 @@ function Blog(props) {
             Sign up
           </Button>
         </Toolbar>
-        <Toolbar className={classes.toolbarMain}>
-          
-        </Toolbar>
         <main>
           {/* Main featured post */}
           <Paper className={classes.mainFeaturedPost}>
             <Grid container>
               <Grid item md={6}>
                 <div className={classes.mainFeaturedPostContent}>
-                  <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                  <Typography
+                    component="h1"
+                    variant="h3"
+                    color="inherit"
+                    gutterBottom
+                  >
                     {title}
                   </Typography>
                   <Typography variant="h5" color="inherit" paragraph>
@@ -146,36 +152,39 @@ function Blog(props) {
           {/* End main featured post */}
           {/* About me */}
           <Grid container spacing={40} className={classes.cardGrid}>
-              <Grid item xs={12} md={12}>
-                <Card className={classes.card}>
-                  <div className={classes.cardDetails}>
-                    <CardContent>
-                      <Typography component="h2" variant="h5">
-                        {about.name}
-                      </Typography>
-                      <Typography variant="subtitle1" color="textSecondary">
-                        {about.title}
-                      </Typography>
-                      <Typography variant="subtitle1" paragraph>
-                        {about.description}
-                      </Typography>
-                      <Typography variant="subtitle1" color="primary">
-                        Continue reading...
-                      </Typography>
-                    </CardContent>
-                  </div>
-                  <Hidden xsDown>
-                    <CardMedia
-                      className={classes.cardMedia}
-                      image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                      title="Image title"
-                    />
-                  </Hidden>
-                </Card>
-              </Grid>
+            <Grid item xs={12} md={12}>
+              <Card className={classes.card}>
+                <div className={classes.cardDetails}>
+                  <CardContent>
+                    <Typography component="h2" variant="h5">
+                      {about.name}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                      {about.title}
+                    </Typography>
+                    <Typography variant="subtitle1" paragraph>
+                      {about.description}
+                    </Typography>
+                  </CardContent>
+                </div>
+                <Hidden xsDown>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+                    title="Image title"
+                  />
+                </Hidden>
+              </Card>
+              <SocialToolbar />
+            </Grid>
           </Grid>
           {/* About me end */}
+
+          <Skills/>
+          
           <CardGrid />
+
+          <VerticalTimeline />
         </main>
       </div>
       {/* Footer */}
@@ -183,7 +192,12 @@ function Blog(props) {
         <Typography variant="h6" align="center" gutterBottom>
           Footer
         </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
           Something here to give the footer a purpose!
         </Typography>
       </footer>
@@ -193,7 +207,7 @@ function Blog(props) {
 }
 
 Blog.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Blog);
